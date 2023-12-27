35d4d33d5f9dde7fc3e9c647e59b43b56ba9b839
theo-lemoine-oc <propjhh>
2023-12-27 18:29:30 +0100

    Fix(Swagger): Fix du swagger-ui

--- src/main/java/com/openclassrooms/rentals/configuration/SwaggerConfig.java
+++ src/main/java/com/openclassrooms/rentals/configuration/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.openclassrooms.rentals.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +16,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
 
     @Bean
     public Docket api() {
